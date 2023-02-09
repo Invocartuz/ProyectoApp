26e294be7f7d19afb2f1bb54fdcefb7f231de3e0
Invocartuz <innova.cristian.maza@gmail.com>
2023-02-08 19:55:08 -0500

    lista con algunas acciones beneficas en version funcion js

---
+++ tutorialesList.js
@@ -0,0 +1,237 @@
+function RecogerBasura() {
+  return "Recoger la basura en un parque o en la calle";
+}
+
+function AyudarPersonaMayor() {
+  return "Ayudar a una persona mayor a hacer compras o a cruzar la calle";
+}
+
+function DonarAlimentos() {
+  return "Donar alimentos o ropa a una organización benéfica";
+}
+
+function OfrecerseVoluntario() {
+  return "Ofrecerse como voluntario en un evento o una organización sin fines de lucro";
+}
+
+function AcompanarAmigo() {
+  return "Acompañar a un amigo o familiar en un momento difícil";
+}
+
+function CocinarCena() {
+  return "Cocinar una cena para personas necesitadas";
+}
+
+function PlantarArboles() {
+  return "Plantar árboles o jardines en un espacio público";
+}
+
+function CuidarAnimales() {
+  return "Ofrecerse para cuidar a los animales de un vecino que está de viaje";
+}
+
+function DonarSangre() {
+  return "Donar sangre";
+}
+
+function OfrecerPalabraAmable() {
+  return "Ofrecer una palabra amable o una sonrisa a alguien que parece tener un mal día";
+}
+
+function AyudarAprenderHabilidad() {
+    return "Ayudar a alguien a aprender una nueva habilidad";
+  }
+  
+  function HacerDonacion() {
+    return "Hacer una donación a una organización que lucha por una causa que te importa";
+  }
+  
+  function LiderarIniciativaComunitaria() {
+    return "Liderar o participar en una iniciativa comunitaria para mejorar un espacio público";
+  }
+  
+  function OfrecerTutoria() {
+    return "Ofrecerse para tutoría o mentoría para jóvenes";
+  }
+  
+  function PrestarLibro() {
+    return "Prestar un libro o una película a un amigo o vecino";
+  }
+  
+  function AyudarOrganizarEvento() {
+    return "Ayudar a organizar un evento benéfico";
+  }
+  
+  function PrestarMano() {
+    return "Prestar una mano a alguien que está mudándose o haciendo reparaciones en su hogar";
+  }
+  
+  function DonarDineroTiempo() {
+    return "Donar dinero o tiempo a una campaña política o una causa que crees importante";
+  }
+  
+  function OrganizarColecta() {
+    return "Organizar una colecta de alimentos o suministros para personas necesitadas";
+  }
+  
+  function OfrecerTrabajosGratuitos() {
+    return "Ofrecerse para hacer trabajos gratuitos para personas de bajos ingresos o ancianos";
+  }
+
+  
+  function PracticarDeportesNiños() {
+    return "Dar a los niños a practicar deportes o a aprender un nuevo juego es una forma maravillosa de mejorar su salud y desarrollo.";
+  }
+  
+  function TareasGratuitasOrganizaciones() {
+    return "Ofrecerse para hacer tareas gratuitas para organizaciones sin fines de lucro es una manera de ayudar a la comunidad y hacer una diferencia positiva en el mundo.";
+  }
+  
+  function AyudaSaludMental() {
+    return "Participar en un grupo de ayuda a personas con problemas de salud mental es una forma de apoyar a aquellos que lo necesitan y mejorar la vida de las personas en la comunidad.";
+  }
+  
+  function PrestarOreja() {
+    return "Prestar una oreja a alguien que necesita hablar es un acto de amabilidad y compasión que puede tener un impacto positivo en la vida de alguien.";
+  }
+  
+  function ComitéLocal() {
+    return "Ofrecerse para ser parte de un comité local o grupo de acción comunitaria es una forma de hacer una diferencia positiva en la comunidad y trabajar hacia un objetivo común.";
+  }
+  
+  function AyudarMoverse() {
+    return "Ayudar a un amigo o vecino a moverse es una forma de ser amable y mostrar apoyo a alguien que lo necesita.";
+  }
+  
+  function LimpiezaPlaya() {
+    return "Organizar una limpieza de playa o de un parque local es una forma de ayudar a proteger el medio ambiente y mantener la belleza natural de nuestra comunidad.";
+  }
+  
+  function GrupoApoyoEnfermedad() {
+    return "Ofrecerse para ser parte de un grupo de apoyo para personas que luchan contra una enfermedad o un trastorno es una forma de proporcionar apoyo y compañía a aquellos que lo necesitan.";
+  }
+  
+  function OfrecerApoyoDifícil() {
+    return "Prestar atención y ofrecer apoyo a alguien que está pasando por un momento difícil es un acto de amabilidad y compasión que puede tener un impacto positivo en la vida de alguien.";
+  }
+
+  function EnseñarHabilidad() {
+    return "Enseñar una habilidad a alguien que quiere aprender";
+}
+
+function OrganizarCaminata() {
+    return "Organizar una caminata o carrera benéfica";
+}
+
+function DonarJuguetes() {
+    return "Donar juguetes o libros a una organización infantil";
+}
+
+function PrestarManoProyecto() {
+    return "Prestar una mano a alguien que está trabajando en un proyecto comunitario";
+}
+
+function AyudarEstudiantes() {
+    return "Ayudar a los estudiantes a prepararse para exámenes o para la universidad";
+}
+
+function OfrecerRefugio() {
+    return "Ofrecer un lugar seguro para personas que necesitan un refugio temporal";
+}
+
+function DonarSuministros() {
+    return "Donar suministros de oficina o electrónicos a una organización sin fines de lucro";
+}
+
+function AyudarDiscapacitado() {
+    return "Ayudar a una persona con discapacidad a hacer tareas cotidianas";
+}
+
+function PrestarCuidadoInfantil() {
+    return "Prestar un servicio de cuidado infantil para que los padres puedan tener una noche libre";
+}
+
+function AyudarMudanza() {
+    return "Ayudar a un amigo o vecino a mudarse";
+}
+
+function OfrecerJardineria() {
+    return "Ofrecerse para hacer un trabajo de jardinería para alguien que no puede hacerlo por sí mismo";
+}
+
+function PrestarVehiculo() {
+    return "Prestar un coche o una bicicleta a alguien que lo necesita";
+}
+
+function AyudarEstudiantes() {
+    return "Ayudar a los estudiantes a prepararse para el colegio o la universidad";
+}
+
+function DonarSuministrosArte() {
+    return "Donar suministros de arte o música a una escuela o una organización sin fines de lucro";
+}
+
+function PrestarReparaciones() {
+    return "Prestar una mano para hacer reparaciones en un hogar o en un edificio comunitario";
+}
+
+function OfrecerConductor() {
+    return "Ofrecerse para ser un conductor de carreras o de eventos benéficos";
+}
+
+function AyudarOrganizar() {
+    return "Ayudar a un amigo o vecino a organizar su hogar o su jardín";
+}
+
+function DonarSuministrosMedicos() {
+    return "Donar suministros médicos a una clínica o un hospital local";
+}
+
+function PrestarConstruccion() {
+    return "Prestar una mano para ayudar a construir un hogar o una escuela para personas necesitadas";
+}
+
+function OfrecerRefugio() {
+    return "Ofrecer un lugar seguro para alojamiento temporal a personas sin hogar";
+}
+
+function DonarSuministrosOficina() {
+    return "Donar suministros de oficina a una organización sin fines de lucro";
+  }
+  
+  function PrestarManoEvento() {
+    return "Prestar una mano para ayudar a una organización sin fines de lucro a llevar a cabo un evento";
+  }
+  
+  function OfrecerseMentor() {
+    return "Ofrecerse para ser un mentor o tutor para jóvenes";
+  }
+  
+  function DonarSuministrosDeportes() {
+    return "Donar suministros de deportes a un equipo o una organización deportiva local";
+  }
+  
+  function PrestarManoSubasta() {
+    return "Prestar una mano para ayudar a organizar una subasta benéfica";
+  }
+  
+  function OfrecerseVoluntarioRefugio() {
+    return "Ofrecerse para ser un voluntario en un refugio de animales";
+  }
+  
+  function DonarSuministrosJardineria() {
+    return "Donar suministros de jardinería a una organización sin fines de lucro";
+  }
+  
+  function PrestarManoFeria() {
+    return "Prestar una mano para ayudar a organizar una feria o un mercado comunitario.";
+  }
+
+  function ayudarTrabajo() {
+    return "Ayudar a una persona a encontrar trabajo o mejorar sus habilidades laborales";
+  }
+  
+  function aprenderIdioma() {
+    return "Ayudar a una persona a aprender un nuevo idioma";
+  }
+  
